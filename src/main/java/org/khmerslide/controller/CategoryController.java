5c8b2aec505d9a7f34214977e9c48b80ae89585d
NemSothea <nemsothea13@gmail.com>
2016-08-09 08:21:01 +0700

    drtgfdyyf

--- src/main/java/org/khmerslide/controller/CategoryController.java
+++ src/main/java/org/khmerslide/controller/CategoryController.java
@@ -18,7 +18,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
-
+//rtfghgf
 @Controller
 @RequestMapping(value="/api/category")
 public class CategoryController {
